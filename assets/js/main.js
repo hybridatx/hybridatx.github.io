40aafe6f81cb22b27611c3568c242fbfabba4211
violetforest <info@violetforest.com>
2021-01-11 19:37:31 -0500

    newsletter

--- assets/js/main.js
+++ assets/js/main.js
@@ -218,13 +218,6 @@
 
 /* 11. Datepicker */
     
-// 11. ---- Mailchimp js --------//  
-    function mailChimp() {
-      $('#mc_embed_signup').find('form').ajaxChimp();
-    }
-    mailChimp();
-
-
 // 12 Pop Up Img
     var popUp = $('.single_gallery_part, .img-pop-up');
       if(popUp.length){
